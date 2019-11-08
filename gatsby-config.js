5c6382c91bc88678647e4d48073b2a598578f3de
Travis Baker <travis_baker@intuit.com>
2019-11-08 13:48:15 -0600

    feature: add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  siteMetadata: {
+    siteUrl: `https://travisbaker.dev`,
+  },
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components', 'gatsby-plugin-sitemap'],
 };
